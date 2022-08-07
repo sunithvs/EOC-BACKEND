ac621a4b92200acec117de99838900211f68844f
sunithvs <sunithvs2002@gmail.com>
2022-08-07 20:43:27 +0530

    added program filter

--- Gallery/admin.py
+++ Gallery/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
 
-from .models import Programs,PostImage
+from .models import Programs, PostImage, Mentor
+
 
 # Register your models here.
 
 class PostImageAdmin(admin.StackedInline):
     model = PostImage
-    
-@admin.register(Programs)
 
+
+@admin.register(Programs)
 class PostAdmin(admin.ModelAdmin):
-    inlines=[PostImageAdmin]
-    
+    inlines = [PostImageAdmin]
+
     class Meta:
         model = Programs
-        
-#@admin.register(PostImage)
+
+
+# @admin.register(PostImage)
 
 class PostImageAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(Mentor)
