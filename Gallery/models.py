a4ed356b64e32fb13318301023272f2b00412323
sunithvs <sunithvs2002@gmail.com>
2022-08-07 11:14:19 +0530

    added files

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 from django.db.models.base import Model
 
+
 # Create your models here.
 
 class Programs(models.Model):
     ProgramID = models.AutoField(primary_key=True)
-    ProgramName =models.CharField(max_length=100)
-    ProgramDesc = models.CharField(max_length=1000)    
+    ProgramName = models.CharField(max_length=100)
+    ProgramDesc = models.CharField(max_length=1000)
     ProgramDate = models.DateField()
-    
-    image = models.FileField(blank=True,upload_to='programs/images/')
+
+    image = models.FileField(blank=True, upload_to='programs/images/')
+    type = models.CharField(max_lngth=200, choices=(("EOC", "EOC"), ("SOCE", "SOCE")),defalt="EOC")
 
     def __str__(self):
         return self.ProgramName
 
+
 class PostImage(models.Model):
-    post = models.ForeignKey(Programs, default=None, on_delete=models.CASCADE,related_name='imagges')
-    images = models.FileField(upload_to = 'programs/images/',)
- 
+    post = models.ForeignKey(Programs, default=None, on_delete=models.CASCADE, related_name='imagges')
+    images = models.FileField(upload_to='programs/images/', )
+
     def __str__(self):
         return self.post.ProgramName
