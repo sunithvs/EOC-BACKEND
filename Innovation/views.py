d713c0c30f803b808a27125db2dfc68963916255
sunith vs <63339782+sunithvs@users.noreply.github.com>
2022-08-11 18:23:51 +0530

    from rest_framework import viewsets, generics

--- Innovation/views.py
+++ Innovation/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
-
+from rest_framework import viewsets, generics
 from Innovation.models import Innovation
 
 from Innovation.serializers import InnovationSerializer
